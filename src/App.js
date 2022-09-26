971fd976ee6bc83e4821ed5e6450623837442b7c
Pratik Jadhav <pratikjadhav9276@gmail.com>
2022-09-26 22:36:10 +0530

    cart with database

---
+++ src/App.js
@@ -0,0 +1,37 @@
+
+import './App.css';
+import React from 'react';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import ScrapMaterial from './pages/ScrapMaterial'
+import Cart from './pages/Cart';
+import ToastContainer from 'react-bootstrap/esm/ToastContainer';
+import 'react-toastify/dist/ReactToastify.css';
+import About from './pages/About';
+
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+      <ToastContainer/>
+      <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/home" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/logi" element={<Login />} />
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path="/scrapMaterial" element={<ScrapMaterial/>}/>
+          <Route path="/about" element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
